eebe9b7e669c1d04096a955307c96b07c8d38f44
Ari Silver <ari.silver@gmail.com>
2022-08-19 10:15:28 -0500

    Commandir: Added Hosting framework/skeleton based on command-line-api/samples/HostingPlayground.

---
+++ src/Commandir/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.CommandLine;
+using System.CommandLine.Builder;
+using System.CommandLine.Hosting;
+using Microsoft.Extensions.Hosting;
+using System.CommandLine.Parsing;
+
+namespace Commandir
+{
+    class Program
+    {
+        static async Task Main(string[] args) => await BuildCommandLine()
+            .UseHost(_ => Host.CreateDefaultBuilder(),
+                host =>
+                {
+                    host.ConfigureServices(services =>
+                    {
+                        //services.AddSingleton<IGreeter, Greeter>();
+                    });
+                })
+            .UseDefaults()
+            .Build()
+            .InvokeAsync(args);
+
+        private static CommandLineBuilder BuildCommandLine()
+        {
+            var root = new RootCommand(@"$ dotnet run --name 'Joe'"){
+                new Option<string>("--name"){
+                    IsRequired = true
+                }
+            };
+            //root.Handler = CommandHandler.Create<GreeterOptions, IHost>(Run);
+            return new CommandLineBuilder(root);
+        }
+
+        // private static void Run(GreeterOptions options, IHost host)
+        // {
+        //     var serviceProvider = host.Services;
+        //     var greeter = serviceProvider.GetRequiredService<IGreeter>();
+        //     var loggerFactory = serviceProvider.GetRequiredService<ILoggerFactory>();
+        //     var logger = loggerFactory.CreateLogger(typeof(Program));
+
+        //     var name = options.Name;
+        //     logger.LogInformation(GreetEvent, "Greeting was requested for: {name}", name);
+        //     greeter.Greet(name);
+        // }
+    }
+}
