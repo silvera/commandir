1116e2f853dc2894264797187c0a1bf639b604d9
Ari Silver <ari.silver@gmail.com>
2022-09-14 13:21:22 -0500

    Removed most unused code from Program.cs. Adding CommandLoadContext.cs so we have it for later.

---
+++ src/Commandir/Builtins/Console.cs
@@ -0,0 +1,12 @@
+namespace Commandir.Builtins;
+
+using System.Threading.Tasks;
+using Commandir.Core;
+
+public class Console : ICommand
+{
+    public Task ExecuteAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/Commandir/Program.cs
+++ src/Commandir/Program.cs
@@ -1,9 +1,13 @@
 ﻿using System.CommandLine;
 using System.CommandLine.Builder;
 using System.CommandLine.Hosting;
+using System.CommandLine.Invocation;
 using System.CommandLine.Parsing;
+using System.Reflection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+
+using Commandir.Core;
 
 namespace Commandir
 {
@@ -19,7 +23,7 @@ namespace Commandir
                     { 
                         host.ConfigureServices(services => 
                         {
-                            services.AddActions();
+                            services.RegisterCommands();
                         });
                     })
                 .UseDefaults()
@@ -34,12 +38,24 @@ namespace Commandir
 
         private static async Task HandleAsync(IHost host)
         {            
-            IActionContextProvider provider = host.Services.GetRequiredService<IActionContextProvider>();
-            CancellationToken cancellationToken = provider.GetCancellationToken();
-            foreach(Action action in provider.GetActions())
-            {
-                await action.ExecuteAsync(cancellationToken);
-            }
+            InvocationContext invocationContext = host.Services.GetRequiredService<InvocationContext>();
+            CommandLineCommand? command = invocationContext.ParseResult.CommandResult.Command as CommandLineCommand;
+            if(command == null)
+                throw new Exception("Failed to obtain command");
+
+            string? commandType  = command.CommandData.Type;
+            if(commandType == null)
+                throw new Exception("Command Type was null");
+
+            ICommandRegistry commandRegistry = host.Services.GetRequiredService<ICommandRegistry>();
+            if(commandRegistry == null)
+                throw new Exception("Command Registry was null");
+
+            ICommand? commandImpl = commandRegistry.GetCommand(commandType);
+            if(commandImpl == null)
+                throw new Exception("Failed to find command impl");
+
+            await commandImpl.ExecuteAsync(); 
         }
 
         private static CommandLineBuilder BuildCommandLine()
@@ -49,12 +65,65 @@ namespace Commandir
             if(!File.Exists(yamlFilePath))
                 throw new FileNotFoundException($"No Commandir.yaml file found in {currentDirectory}", "Commandir.yaml");
 
-            StreamReader yamlFileReader = new StreamReader(yamlFilePath);
-            IBuilder<CommandData> commandDataBuilder = new YamlCommandDataBuilder(yamlFileReader);
-            CommandData rootCommandData = commandDataBuilder.Build();
-            IBuilder<Command> commandBuilder = new CommandBuilder(rootCommandData, HandleAsync);
-            Command rootcommand = commandBuilder.Build();
-            return new CommandLineBuilder(rootcommand);
+            string yaml = File.ReadAllText(yamlFilePath);
+            Core.CommandData rootData  = new YamlCommandDataBuilder2(yaml).Build();
+            CommandLineCommand rootCommand = new CommandBuilder(rootData, HandleAsync).Build(); 
+            return new CommandLineBuilder(rootCommand);
+        }
+    }
+
+    public static class ServiceExtensions
+    {
+        public static IServiceCollection RegisterCommands(this IServiceCollection services)
+        {
+            CommandRegistry registry = new CommandRegistry();
+            registry.RegisterCommands(typeof(Program).Assembly);
+            services.AddSingleton(typeof(ICommandRegistry), sp => registry);
+            return services;
+        }
+    }
+
+    public interface ICommandRegistry
+    {
+        void RegisterCommands(Assembly assembly);
+        ICommand? GetCommand(string fullName);
+    }
+
+    public class CommandRegistry : ICommandRegistry
+    {
+        private readonly Dictionary<string, ICommand> _commands = new Dictionary<string, ICommand>(StringComparer.OrdinalIgnoreCase);
+        
+        public void RegisterCommands(Assembly assembly)
+        {
+            List<ICommand> commands = CreateCommands(assembly);
+            foreach(ICommand command in commands)
+            {
+                string? fullName = command.GetType().FullName;
+                if(fullName != null)
+                    _commands[fullName] = command;
+            }
+        }
+
+        private static List<ICommand> CreateCommands(Assembly assembly)
+        {
+            List<ICommand> commands = new List<ICommand>();
+            foreach (Type type in assembly.GetTypes())
+            {
+                if (typeof(ICommand).IsAssignableFrom(type))
+                {
+                    ICommand? command = Activator.CreateInstance(type) as ICommand;
+                    if (command != null)
+                    {
+                        commands.Add(command);
+                    }
+                }
+            }
+            return commands;
+        }
+
+        public ICommand? GetCommand(string fullName)
+        {
+            return _commands.TryGetValue(fullName, out ICommand? command) ? command : null;
         }
     }
 }
