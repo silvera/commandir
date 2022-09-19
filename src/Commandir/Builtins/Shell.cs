a4b78eae7ac7922741ebc02c7621020b5476f4c1
Ari Silver <ari.silver@gmail.com>
2022-09-19 15:58:27 -0500

    Added Shell command. Updated Yaml file to support both commands, without requiring command-line arguments.

---
+++ src/Commandir/Builtins/Shell.cs
@@ -0,0 +1,49 @@
+using Commandir.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace Commandir.Builtins;
+
+public class Shell : ICommand
+{
+    public async Task ExecuteAsync(ICommandContext context)
+    {
+        var loggerFactory = context.Services.GetRequiredService<ILoggerFactory>();
+        var logger = loggerFactory.CreateLogger<Shell>();
+
+        if(!context.Parameters.TryGetValue("command", out object? commandObj))
+            throw new Exception($"Failed to find parameter `command`.");
+        
+        string? command = Convert.ToString(commandObj);
+        if(command == null)
+            throw new Exception("Failed convert parameter `command` to a string.");
+
+        string shell = "bash";
+
+        // Create a temporary file.
+        string tempFile = Path.GetTempFileName();
+        
+        // Write the contents of the command to the file.
+        using (var writer = new StreamWriter(tempFile))
+        {
+            writer.WriteLine(command);
+            logger.LogInformation("Wrote command: {Command} to file: {TempFile}", command, tempFile);
+        }
+
+        using var process = Process.Start(new ProcessStartInfo
+        {
+            UseShellExecute = false,
+            FileName = shell,
+            ArgumentList = { tempFile }
+        });
+        if(process == null)
+            throw new Exception($"Failed to create process: {shell} with arguments: {tempFile}");
+
+        logger.LogInformation("Executing command: {Command}", command);
+        await process.WaitForExitAsync();
+
+        logger.LogInformation("Deleting file: {TempFile}", tempFile);
+        File.Delete(tempFile);
+    }
+}
