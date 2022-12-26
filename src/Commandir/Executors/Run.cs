aaea5fab356ea188d348b41b3706efcd399ca953
Ari Silver <ari.silver@gmail.com>
2022-12-26 11:23:02 -0600

    Abstracted default command running logic based on OS. Created a default BashRunner for Linux and a default CmdRunner for Windows. Closes #9.

--- src/Commandir/Executors/Run.cs
+++ src/Commandir/Executors/Run.cs
@@ -5,17 +5,24 @@ using System.Runtime.InteropServices;
 
 namespace Commandir.Executors;
 
-/// <summary>
-/// Runs the command speciifed by the 'command' parameter.
-/// </summary>
-internal sealed class Run : IExecutor
+internal abstract class RunnerBase
 {
-    public async Task<object?> ExecuteAsync(IExecutionContext context)
+    private readonly string _runner;
+    private readonly string _runnerExtension;
+    private readonly string _runnerFlags;
+    protected RunnerBase(string runner, string runnerExtension, string runnerFlags)
     {
-        ILogger logger = context.LoggerFactory.CreateLogger<Run>();
-        logger.LogInformation("Executing command: {CommandPath}", context.Path);
+        _runner = runner;
+        _runnerExtension = runnerExtension;
+        _runnerFlags = runnerFlags;
+    }
 
-        object? commandObj = context.ParameterContext.GetParameterValue("command");
+    public async Task<int> RunAsync(IExecutionContext executionContext)
+    {
+        ILogger logger = executionContext.LoggerFactory.CreateLogger<Run>();
+        logger.LogInformation("Executing command: {CommandPath}", executionContext.Path);
+
+        object? commandObj = executionContext.ParameterContext.GetParameterValue("command");
         if(commandObj is null)
             throw new Exception("Failed to find parameter `command`");
 
@@ -23,31 +30,26 @@ internal sealed class Run : IExecutor
         if(command == null)
             throw new Exception("Failed convert parameter `command` to a string.");
 
-        string formattedCommand = context.ParameterContext.FormatParameters(command);
+        string formattedCommand = executionContext.ParameterContext.FormatParameters(command);
 
-        // Determine the current OS and use the default runner for each (if no 'runner' parameter is specified by the user).
-        // Linux/MacOS/Others: bash
-        // Windows: cmd.exe
+        logger.LogInformation("Runner: {Runner} Command: {Command}", _runner, formattedCommand);
 
-        string runner = "bash";
-        if(RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-        {
-            runner = "cmd.exe";
-        }
-
-        logger.LogInformation("Runner: {Runner} Command: {Command}", runner, formattedCommand);
-
+        // Command needs to be written to a file executed by the specified runner.
         // Create a new file in the current directory.
-        string scriptFileName = context.Path.Replace("/", "_").TrimStart('_') + ".cmd";
-        string scriptFilePath = Path.Combine(Directory.GetCurrentDirectory(), scriptFileName);
+        string runnerFileName = executionContext.Path
+            .Replace("/", "_")
+            .TrimStart('_') 
+            + _runnerExtension;
+        
+        string runnerFilePath = Path.Combine(Directory.GetCurrentDirectory(), runnerFileName);
         
         // Write the contents of the command to the file.
-        logger.LogInformation("Creating file: {ScriptPath}", scriptFilePath);
+        logger.LogInformation("Creating file: {RunnerFile}", runnerFileName);
         
-        using (var writer = new StreamWriter(scriptFilePath))
+        using (var writer = new StreamWriter(runnerFilePath))
         {
             writer.WriteLine(formattedCommand);
-        }
+        }    
 
         try
         {
@@ -55,30 +57,74 @@ internal sealed class Run : IExecutor
             {   CreateNoWindow = true,
                 UseShellExecute = false,
                 WorkingDirectory = Directory.GetCurrentDirectory(),
-                FileName = runner,
-                ArgumentList = { "/c", scriptFilePath }
+                FileName = _runner,
             };
+
+            // Add runner flags (if any)
+            if(!string.IsNullOrWhiteSpace(_runnerFlags))
+            {
+                processStartInfo.Arguments = _runnerFlags;
+            }
             
-            string commandLine = $"{runner} {string.Join(" ", processStartInfo.ArgumentList)}";
+            // Add script file
+            processStartInfo.ArgumentList.Add(runnerFilePath);
+
+            string commandLine = $"{_runner} {string.Join(" ", processStartInfo.ArgumentList)}";
             logger.LogInformation("Process Starting: {CommandLine}", commandLine);
             var process = Process.Start(processStartInfo);
             if(process == null)
                 throw new Exception($"Failed to create process `{commandLine}`");
     
-            await process.WaitForExitAsync(context.CancellationToken);
+            await process.WaitForExitAsync(executionContext.CancellationToken);
             int exitCode = process.ExitCode;
             logger.LogInformation("Process Complete. ExitCode: {ExitCode}", exitCode);
             return process.ExitCode;
         }
         catch(Exception e)
         {
-            logger.LogError(e, "Exeception while exeucting command: {Command}", formattedCommand);
+            logger.LogError(e, "Exeception while exeucting command: {Command}", command);
             return -1;
         }
         finally
         {
-            logger.LogInformation("Deleting file: {ScriptPath}", scriptFilePath);
-            File.Delete(scriptFilePath);
+            logger.LogInformation("Deleting file: {ScriptPath}", runnerFilePath);
+            File.Delete(runnerFilePath);
         }
+    }
+}
+
+internal sealed class BashRunner : RunnerBase
+{
+    public BashRunner()
+        : base("bash", ".sh", string.Empty)
+    {
+    }
+}
+
+internal sealed class CmdRunner : RunnerBase
+{
+    public CmdRunner()
+        : base("cmd.exe", ".cmd", "/c")
+    {
+    }
+}
+
+
+/// <summary>
+/// Runs the command specified by the 'command' parameter.
+/// </summary>
+internal sealed class Run : IExecutor
+{
+    public async Task<object?> ExecuteAsync(IExecutionContext context)
+    {
+        // TODO: Look for a user-defined runner, runnerFlags etc.
+        // Determine the current OS and use the default runner for each.
+        // Linux/MacOS/Others: bash
+        // Windows: cmd.exe
+        RunnerBase runner = RuntimeInformation.IsOSPlatform(OSPlatform.Windows)
+        ? new CmdRunner()
+        : new BashRunner();
+
+        return await runner.RunAsync(context);
     }
 }
