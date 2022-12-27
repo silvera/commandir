c4893fef01f79ce6c089e850d97a96723a7c6a50
Ari Silver <ari.silver@gmail.com>
2022-12-27 11:05:47 -0600

    Tests: Changing windows script file extension from cmd to bat to see if that fixes the invocation issue.

--- src/Commandir.Tests/ScriptExecutionTests.cs
+++ src/Commandir.Tests/ScriptExecutionTests.cs
@@ -37,11 +37,11 @@ public class ScriptExecutionTests : TestsBase
                     - name: relative-path
                       executor: commandir.executors.run
                       parameters:
-                         command: CALL hello_world.cmd > {file1}
+                         command: CALL hello_world.bat > {file1}
                     - name: absolute-path
                       executor: commandir.executors.run
                       parameters:
-                         command: CALL {currentDirectory}/hello_world.cmd > {file1}
+                         command: CALL {currentDirectory}/hello_world.bat > {file1}
         ";
     }
 
